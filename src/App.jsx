7344ebba08e6beac8de268d071c1de37a5d065d0
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-12-10 19:52:35 +0530

    Update App with HeroSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header.jsx";
+import HeroSection from "./components/HeroSection.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <HeroSection />
+      </main>
     </>
   );
 }
