1b5a4ad4688f62e0b83186468c832b221546130f
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-12-10 18:06:29 +0530

    Update tailwind config with gradient colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orangeGradientStart: "#FF591E",
+        orangeGradientEnd: "#FB432C",
+      },
+    },
   },
   plugins: [],
 };
